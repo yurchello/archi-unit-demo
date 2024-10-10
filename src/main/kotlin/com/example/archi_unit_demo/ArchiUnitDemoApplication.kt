867fc60d70aff4aea46782d5ebd0222791bf4101
Yuriy_Mezentsev <yuriy_mezentsev@epam.com>
2024-10-10 17:23:39 +0200

    initial commit

---
+++ src/main/kotlin/com/example/archi_unit_demo/ArchiUnitDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.archi_unit_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchiUnitDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchiUnitDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/archi_unit_demo/ArchiUnitDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.archi_unit_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchiUnitDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
